151361e1f7f8536ba5f79ba082baa22fe9237b29
THPdmontes <niner90@ciencias.unam.mx>
2016-02-17 18:34:15 -0600

    Se incluye el código para poder reconocer y generar bytecode para el operador /

---
+++ Clase17.02/j--/tests/pass/Division.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+
+public class Division{
+
+    public int divide(int a, int b){
+	return a/b;
+    }
+
+    public static void main(String[] args){
+	Division prueba = new Division();
+	int resultado = 5-21/4*5/8+2;
+	System.out.println(resultado);
+    }
+}
