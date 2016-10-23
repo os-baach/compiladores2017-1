af153147e79302a4f011aa9d3000f0af2a321cfa
rickjmzmnz <rickjmzmnz@ciencias.unam.mx>
2016-10-22 22:30:08 -0500

    Borré archivos de la práctica 2 de sobra

--- Practicas/Practica2/ParserTokens.java
+++
@@ -1,7 +0,0 @@
-public interface ParserTokens {
-public final static short NUMBER=257;
-public final static short MAS=258;
-public final static short MENOS=259;
-public final static short MULT=260;
-public final static short DIV=261;
-}
