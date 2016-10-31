2f9695062e77a7ad185b948e4eeaba712a955873
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-10-30 20:42:46 -0600

    Agrega métodos que se piden en el punto 1 de los ejercicios para llevar

--- Practicas/Practica3/Nodo.java
+++ Practicas/Practica3/Nodo.java
@@ -1,11 +1,39 @@
 /* Clase principal de Nodos(Componente(?)) */
 public class Nodo{
     int value; /* Valor dentro del nodo, aunque no entiendo por qué es int */
+    Nodo izq; /* Hijo izquierdo */
+    Nodo der; /* Hijo derecho */
     
     /* Regresa el valor del nodo */
     public int getValue(){
 	return value;
     }
+
+    /* Establece para el nodo un nuevo valor (sólo para hojas) */
+    public void setValue(int val){
+	this.value = val;
+    }
+
+    /* Regresa al hijo derecho del Nodo */
+    public Nodo getHijoIzq(){
+	return this.izq;
+    }
+
+    /* Regresa al hijo derecho del Nodo */
+    public Nodo getHijoDer(){
+	return this.der;
+    }
+
+    /* Establece un nuevo hijo izquierdo para el Nodo */
+    public void setHijoIzq(Nodo i){
+	this.izq = i;
+    }
+
+    /* Establece un nuevo hijo derecho para el Nodo */
+    public void setHijoDer(Nodo d){
+	this.der = d;
+    }
+    
 }
 
 /* Clase Hoja. Vacía por ahora. */
